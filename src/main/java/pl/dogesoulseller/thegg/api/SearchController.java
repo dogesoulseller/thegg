712395d29c5f3a0f788de0d199bfd6c33c3a4585
Marcin Czerwonka <mcmarcinoser5@gmail.com>
2021-02-21 22:37:40 +0100

    Stub out basic search API method

--- src/main/java/pl/dogesoulseller/thegg/api/SearchController.java
+++ src/main/java/pl/dogesoulseller/thegg/api/SearchController.java
@@ -1,8 +1,18 @@
 package pl.dogesoulseller.thegg.api;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+import pl.dogesoulseller.thegg.api.model.Post;
 
 @RestController
 public class SearchController {
-
+	@GetMapping("/api/search")
+	public ResponseEntity<Post[]> performSearch(@RequestParam(required = false) String query) {
+		throw new ResponseStatusException(HttpStatus.NOT_IMPLEMENTED);
+	}
 }
