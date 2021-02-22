0bd3fae04876070c8708734a208dd54dc42d282a
Marcin Czerwonka <mcmarcinoser5@gmail.com>
2021-02-22 18:22:32 +0100

    Remove unnecessary method in post repo

--- src/main/java/pl/dogesoulseller/thegg/repo/MongoPostRepository.java
+++ src/main/java/pl/dogesoulseller/thegg/repo/MongoPostRepository.java
@@ -1,14 +1,13 @@
 package pl.dogesoulseller.thegg.repo;
 
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import pl.dogesoulseller.thegg.api.model.Post;
 
 public interface MongoPostRepository extends MongoRepository<Post, String> {
-	Optional<Post> findById(String id);
 	List<Post> findByMime(String mime);
+
 	List<Post> findByParent(Post parent);
 }
