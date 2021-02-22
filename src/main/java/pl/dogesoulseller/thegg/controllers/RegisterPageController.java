3c762b28b331a0d8c923d2c1d8e4774c46a79026
Marcin Czerwonka <mcmarcinoser5@gmail.com>
2021-02-22 18:21:55 +0100

    Rename page controllers

--- src/main/java/pl/dogesoulseller/thegg/controllers/RegisterPageController.java
+++ src/main/java/pl/dogesoulseller/thegg/controllers/RegisterPageController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import pl.dogesoulseller.thegg.api.model.UserRegister;
 
 @Controller
-public class RegisterController {
+public class RegisterPageController {
 	@GetMapping("/register")
 	public String register(Model model) {
 		var regInfo = new UserRegister();
