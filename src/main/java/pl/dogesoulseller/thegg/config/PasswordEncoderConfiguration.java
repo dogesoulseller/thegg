00ba8d32626e1034e6631adcc7b51def3db3d473
Marcin Czerwonka <mcmarcinoser5@gmail.com>
2021-03-24 12:24:54 +0100

    Allow changing password hashing params from properties

--- src/main/java/pl/dogesoulseller/thegg/config/PasswordEncoderConfiguration.java
+++ src/main/java/pl/dogesoulseller/thegg/config/PasswordEncoderConfiguration.java
@@ -1,5 +1,6 @@
 package pl.dogesoulseller.thegg.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
@@ -7,8 +8,14 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class PasswordEncoderConfiguration {
+	@Value("${authentication.argon.memory-pow}")
+	Integer memoryPower;
+
+	@Value("${authentication.argon.iterations}")
+	Integer iterations;
+
 	@Bean
 	public PasswordEncoder passwordEncoder() {
-		return new Argon2PasswordEncoder(16, 32, 1, 1 << 16, 4);
+		return new Argon2PasswordEncoder(16, 32, 1, 1 << memoryPower, iterations);
 	}
 }
